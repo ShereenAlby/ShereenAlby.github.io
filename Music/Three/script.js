4bbdd85fb00ebbd4bbf83d4bd0d8f799e4d960be
Shereen Alby <71765313+ShereenAlby@users.noreply.github.com>
2021-12-26 09:41:49 +0530

    Update script.js

--- Music/Three/script.js
+++ Music/Three/script.js
@@ -14,7 +14,7 @@ let songs = [
     {songName: "Forever", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "Springday", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "Magical", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Mikrokosmo", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Kosmos", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Soul", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "So What", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
     {songName: "Stay", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
